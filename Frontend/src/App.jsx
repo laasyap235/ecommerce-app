99b5f4e4dd291715ed9cd6b100adc7296f544e32
maaz78-eng <mohammed.m@waferwire.com>
2026-06-23 11:27:01 +0530

    Pushing Initial Home Page Build

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import EcommercePage from './Components/EcommerceHome'
+function App() {
+
+  return (
+    <>
+    <EcommercePage/>
+    </>
+  )
+}
+
+export default App
